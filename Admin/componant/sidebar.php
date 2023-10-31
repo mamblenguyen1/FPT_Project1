c7260a11632bcd54b81a93d58f5969886b47b367
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2023-10-31 17:26:46 +0700

    update user

--- Admin/componant/sidebar.php
+++ Admin/componant/sidebar.php
@@ -152,6 +152,24 @@
           </ul>
         </li>
 
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="nav-icon fas fa-copy"></i>
+            <p>
+              Tài khoản
+              <i class="fas fa-angle-left right"></i>
+              <span class="badge badge-info right">6</span>
+            </p>
+          </a>
+          <ul class="nav nav-treeview">
+            <li class="nav-item">
+              <a href="?pages=admin&action=UserList" class="nav-link">
+                <i class="far fa-circle nav-icon"></i>
+                <p>Tất cả tài Khoản</p>
+              </a>
+            </li>
+          </ul>
+        </li>
 
         <li class="nav-item">
           <a href="#" class="nav-link">
