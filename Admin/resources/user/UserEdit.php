10156fe90a5ce642717633ee27a70534aaca79cf
XiGumCuNe <xigumcune@gmail.com>
2023-10-31 16:44:48 +0700

    DA1_28_Trang thêm, cập nhật trạng thái tài khoản, xóa người dùng

---
+++ Admin/resources/user/UserEdit.php
@@ -0,0 +1,39 @@
+<?php include './Admin/componant/header.php' ?>
+
+<?php include './admin/componant/sidebar.php' ?>
+
+<div class="main-panel">
+    <div class="content-wrapper">
+        <div class="row">
+            <h1 style="padding-left: 30px;">Chỉnh Sửa Tài Khoản</h1>
+        </div>
+        <div class="add-cate-form">
+        <div class="card card-primary">
+              <div class="card-header"></div>
+              <!-- /.card-header -->
+              <!-- form start -->
+              <form>
+                <div class="card-body">
+                  <div class="form-group">
+                    <label for="exampleInputEmail1">Địa chỉ email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                  </div>
+                  <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+<?php include './admin/componant/footer.php' ?>
