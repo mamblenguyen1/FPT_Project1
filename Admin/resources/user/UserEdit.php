93864fbb115c3008afee3e0254dccd574aa1ab83
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2023-11-01 00:44:24 +0700

    bắt lỗi user

--- Admin/resources/user/UserEdit.php
+++ Admin/resources/user/UserEdit.php
@@ -49,27 +49,67 @@ if (isset($_POST['sua_user'])) {
               <label for="exampleInputEmail1">Tên người dùng</label>
               <input type="text" class="form-control" id="exampleInputEmail1" name="user_name" value="<? echo $user->getInfouser($user_id, 'user_name'); ?>">
             </div>
-
+            <?
+            if (isset($_POST["user_name"])) {
+              if (empty($_POST["user_name"])) {
+                echo '<span class="vaild">Xin vui lòng nhập tên người dùng</span>';
+              } else {
+                echo '';
+              }
+            }
+            ?>
             <div class="form-group">
               <label for="exampleInputEmail1">Password</label>
               <input type="text" class="form-control" id="exampleInputEmail1" name="user_password" value="<? echo $user->getInfouser($user_id, 'user_password'); ?>">
             </div>
-
+            <?
+            if (isset($_POST["user_password"])) {
+              if (empty($_POST["user_password"])) {
+                echo '<span class="vaild">Xin vui lòng nhập password</span>';
+              } else {
+                echo '';
+              }
+            }
+            ?>
             <div class="form-group">
               <label for="exampleInputEmail1">Số điện thoại</label>
               <input type="text" class="form-control" id="exampleInputEmail1" name="user_phone_number" value="<? echo $user->getInfouser($user_id, 'user_phone_number'); ?>">
             </div>
-
+            <?
+            if (isset($_POST["user_phone_number"])) {
+              if (empty($_POST["user_phone_number"])) {
+                echo '<span class="vaild">Xin vui lòng nhập số điện thoại</span>';
+              } else {
+                echo '';
+              }
+            }
+            ?>
             <div class="form-group">
               <label for="exampleInputEmail1">Địa chỉ</label>
               <input type="text" class="form-control" id="exampleInputEmail1" name="user_address" value="<? echo $user->getInfouser($user_id, 'user_address'); ?>">
             </div>
-
+            <?
+            if (isset($_POST["user_address"])) {
+              if (empty($_POST["user_address"])) {
+                echo '<span class="vaild">Xin vui lòng nhập địa chỉ</span>';
+              } else {
+                echo '';
+              }
+            }
+            ?>
             <div class="form-group">
               <label for="exampleInputPassword1">Vai trò</label>
               <input type="text" class="form-control" id="exampleInputPassword1" name="role_id" value="<? echo $user->getInfouser($user_id, 'role_id'); ?>">
             </div>
-
+            <?
+            if (isset($_POST["role_id"])) {
+              if (empty($_POST["role_id"])) {
+                echo '<span class="vaild">Xin vui lòng nhập vai trò</span>';
+              } else {
+                echo '';
+              }
+            }
+            ?>
           </div>
           <!-- /.card-body -->
 
