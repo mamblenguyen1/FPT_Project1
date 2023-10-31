d76261fb2ce43c19224bd794ae355034a180ec76
mamblenguyen1 <quangnmpc07626@fpt.edu.vn>
2023-10-31 11:57:28 +0700

    Update Thông Tin

--- Admin/resources/comment/CommentDetail.php
+++ Admin/resources/comment/CommentDetail.php
@@ -5,22 +5,22 @@
 <div class="main-panel">
     <div class="content-wrapper">
         <div class="row">
-            <h1 style="padding-left: 30px;">Danh sách danh mục</h1>
+            <h1 style="padding-left: 30px;">Danh sách chi tiết bình luận</h1>
         </div>
         <div class="container-fluid">
             <div class="row">
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Danh mục danh mục cửa hàng</h3>
+                            <h3 class="card-title">Các bình luận về sản phẩm <strong>Iphone 15 Promax</strong></h3>
                         </div>
                         <!-- /.card-header -->
                         <div class="card-body">
                             <table id="example2" class="table table-bordered table-hover">
                                 <thead>
                                     <tr>
-                                        <th>Tên Danh mục</th>
-                                        <th>Số loại danh mục</th>
+                                        <th>Tài khoản bình luận</th>
+                                        <th>Ngày bình luận</th>
                                         <th>Số lượng tất cả sản phẩm</th>
                                         <th>Thời gian tạo</th>
                                         <th>Thao tác</th>
