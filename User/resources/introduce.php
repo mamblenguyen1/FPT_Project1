3e3a1f6e4626d3f7400d30a2ea6a99cd2806f485
dang12e <ctytnhh5thanhvienvogiacu@gmail.com>
2023-11-02 23:31:32 +0700

    cập nhật trang giới thiệu

--- User/resources/introduce.php
+++ User/resources/introduce.php
@@ -7,7 +7,6 @@ include('user/component/header.php');
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Heebo:wght@400;500;600&family=Nunito:wght@600;700;800&display=swap" rel="stylesheet">
-<!-- Icon Font Stylesheet -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
 <!-- Template Stylesheet -->
